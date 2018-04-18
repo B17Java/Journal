cacc59cc7f1c864430f763a4ba7cc599814781a3
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2018-04-18 21:16:07 +0300

    Create struct

--- .idea/src/com/journal/model/Student.java
+++
@@ -1,8 +0,0 @@
-package com.journal.model;
-/*
-Aleksandr Kuzmenko
- */
-public class Student {
-    private String firstname;
-
-}
---
+++ src/com/journal/model/Student.java
@@ -0,0 +1,12 @@
+package com.journal.model;
+/*
+Aleksandr Kuzmenko
+ */
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+}
