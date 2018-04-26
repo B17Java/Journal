90f2cd6b4b9c3584b2523670a1c122d524dd48cd
Volodymyr <vlad5855051@gmail.com>
2018-04-26 15:37:50 +0300

    update

--- src/com/journal/model/Group.java
+++ src/com/journal/model/Group.java
@@ -6,4 +6,43 @@ public class Group {
     private long[] idStudents;
     private String startData;
     private String endData;
+
+    public Group(long id, String groupName, String startData, String endData) {
+        this.id = id;
+        this.groupName = groupName;
+        this.startData = startData;
+        this.endData = endData;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getStartData() {
+        return startData;
+    }
+
+    public void setStartData(String startData) {
+        this.startData = startData;
+    }
+
+    public String getEndData() {
+        return endData;
+    }
+
+    public void setEndData(String endData) {
+        this.endData = endData;
+    }
 }
--- src/com/journal/model/Lesson.java
+++ src/com/journal/model/Lesson.java
@@ -5,4 +5,41 @@ public class Lesson {
     private String lessonName;
     private Room lessonRoom;
     private long idGroup;
+
+    public Lesson(long id, String lessonName) {
+        this.id = id;
+        this.lessonName = lessonName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public Room getLessonRoom() {
+        return lessonRoom;
+    }
+
+    public void setLessonRoom(Room lessonRoom) {
+        this.lessonRoom = lessonRoom;
+    }
+
+    public long getIdGroup() {
+        return idGroup;
+    }
+
+    public void setIdGroup(long idGroup) {
+        this.idGroup = idGroup;
+    }
 }
