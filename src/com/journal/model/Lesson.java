1116b07e8a056233633c45e9a38ca66b7e8eddf5
Alex <alexey8800@gmail.com>
2018-04-30 22:24:03 +0300

    Merge branch 'master' of C:\Users\HP4330s\OneDrive\_IT_Academy\Programming\_Java\Journal with conflicts.

--- src/com/journal/model/Lesson.java
+++ src/com/journal/model/Lesson.java
@@ -15,9 +15,7 @@ public class Lesson {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    public void setId(long id) { this.id = id; }
 
     public String getLessonName() {
         return lessonName;
