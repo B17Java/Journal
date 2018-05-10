df60788453d0f342f8a938d786e7cc84f209b331
B17Java <38086137+B17Java@users.noreply.github.com>
2018-05-10 18:53:13 +0300

    Update Main.java

--- src/com/journal/Main.java
+++ src/com/journal/Main.java
@@ -2,9 +2,7 @@ package com.journal;
 
 
 
-import com.journal.model.Person;
-import com.journal.repository.interf.PersonRepository;
-import com.journal.repository.impl.PersonRepositoryList;
+
 
 public class Main {
     public static void main(String[] args) {
