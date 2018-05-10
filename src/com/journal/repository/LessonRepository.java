d49a83edbd9f00ad641c2584d4d7edcf3db7de10
Alex <alexey8800@gmail.com>
2018-05-10 20:00:53 +0300

    Add LessonRepositoryMap

--- src/com/journal/repository/LessonRepository.java
+++ src/com/journal/repository/LessonRepository.java
@@ -5,6 +5,6 @@ import com.journal.model.Lesson;
 public interface LessonRepository {
     boolean create (Lesson lesson);
     Lesson read (Integer id);
-    boolean updete (Lesson lesson);
+    boolean update (Lesson lesson);
     boolean delete (Integer id);
 }
